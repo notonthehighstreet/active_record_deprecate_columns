0006afe4975f93f74ec4e4f21ab8b5a312dca2c5
Alex Jahraus, Alex Sunderland and Paul Robinson <alexanderjahraus+alexsunderland+paulrobinson@notonthehighstreet.com>
2015-08-21 17:46:31 +0100

    Make deprecate_column more robust by using define_method
    Fixes an issue where an AR object still had access to a deprecated column
    when it was loaded from the database.
    
    deprecate_column accepts two optional named parameters:
    
      with_block: (Proc) defaults to nil
                  Proc to be called when the deprecated column is accessed
    
      fallback: (Symbol) defaults to nil
                name of other method to call instead of the deprecated one

--- spec/models.rb
+++ spec/models.rb
@@ -2,10 +2,14 @@ class BrandNewObject < ActiveRecord::Base
 end
 
 class User < ActiveRecord::Base
-  deprecate_column :old_name
-  deprecate_column "old_name_as_string"
+  attr_accessor :attribute
+  deprecate_column :nothing
+  deprecate_column :old_name, with_block: Proc.new { raise NoMethodError }
+  deprecate_column "with_block", with_block: Proc.new { raise "specific error" }
+  deprecate_column "with_fallback", fallback: :name
+  deprecate_column :with_block_and_fallback, with_block: Proc.new {|object| object.attribute = "SET" }, fallback: :attribute
 end
 
 class Vehicle < ActiveRecord::Base
-  deprecate_column :not_used
+  deprecate_column :not_used, with_block: Proc.new { raise NoMethodError }
 end
